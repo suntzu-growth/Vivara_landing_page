1fc4dc57b7ca85c774230b4d1c493b1010a1bb93
matias-suntzu <matias@suntzu-growth.com>
2026-02-04 02:21:22 +0100

    implementacion de imagenes 2

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
     const authSession = request.cookies.get('auth_session')
     const isLoginPage = request.nextUrl.pathname === '/login'
 
